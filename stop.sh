5dda29e2ea61b613f83d8a9cbce8ea34d05736ad
Tobias Blaufuß <t.blaufuss@gmx.de>
2019-06-13 23:50:25 +0200

    [UPDATE] cleanup of .pid file

--- stop.sh
+++ stop.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [[ ! -f "app.pid" ]]; then
-    echo "app.pid does not exist. Stopping not possible."
+FILE="app.pid"
+
+if [[ ! -f "$FILE" ]]; then
+    echo "$FILE does not exist. Stopping not possible."
     exit 1
 fi
-kill $(cat app.pid)
+kill $(cat $FILE)
+rm $FILE
